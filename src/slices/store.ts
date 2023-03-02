f9d3a839da5684e608a9c164c13d020ae3108e51
Jerome Cukier <jeromecukier+github@gmail.com>
2023-03-02 12:19:38 -0800

    adds custom hook

--- src/slices/store.ts
+++ src/slices/store.ts
@@ -12,6 +12,7 @@ const makeStore = () =>
 
   export type AppStore = ReturnType<typeof makeStore>;
   export type AppState = ReturnType<AppStore["getState"]>;
+  export type AppDispatch = AppStore["dispatch"];
   export type AppThunk<ReturnType = void> = ThunkAction<
     ReturnType,
     AppState,
