4ccd4701cf3a6ee45cc8684c82554ca169c6ba66
Jerome Cukier <jeromecukier+github@gmail.com>
2023-03-07 16:48:56 -0800

    restyle sidebar for better colors

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,4 +1,4 @@
-import { Fira_Code, Open_Sans } from '@next/font/google';
+import { Fira_Code, Open_Sans } from 'next/font/google';
 export const firaCode = Fira_Code({ subsets: ['latin'] });
 export const openSans = Open_Sans({ subsets: ['latin'] });
 export const withFiraCode = (className: string): string => `${className} ${firaCode.className}`;
